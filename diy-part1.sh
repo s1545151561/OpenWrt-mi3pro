70073000df2de06b42f6d1315ae55a541d56ee4c
s1545151561 <141800803+s1545151561@users.noreply.github.com>
2024-08-18 20:14:16 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,7 +16,7 @@
 # Add a feed source
  echo >> feeds.conf.default
  echo 'src-git istore https://github.com/linkease/istore;main' >> feeds.conf.default
-  echo >> feeds.conf.default
-  echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
-  echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
+ echo >> feeds.conf.default
+ echo 'src-git nas https://github.com/linkease/nas-packages.git;master' >> feeds.conf.default
+ echo 'src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' >> feeds.conf.default
 
